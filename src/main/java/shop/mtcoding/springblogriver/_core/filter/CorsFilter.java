910ce4788d962750152a5f28abb11ead9aec18a2
kangho.lee.pr <kangho.lee.pr@gmail.com>
2025-01-09 10:28:55 +0900

    아마 최종 api 서버?

--- src/main/java/shop/mtcoding/springblogriver/_core/filter/CorsFilter.java
+++ src/main/java/shop/mtcoding/springblogriver/_core/filter/CorsFilter.java
@@ -14,19 +14,20 @@ public class CorsFilter implements Filter {
         HttpServletRequest request = (HttpServletRequest) req;
         HttpServletResponse response = (HttpServletResponse) res;
 
-        String origin = request.getHeader("Origin"); // 주소(포트)가 계속 바뀌니까 요청 때 온 주소를 헤더에서 받아옴
+        // 안드로이드 스튜디오 디버그 크롬으로 할수 있게
+        String origin = request.getHeader("Origin"); // 주소(포트)가 계속 바뀌니까 요청 때 온 주소를 헤더에서 받아옴, 임시조치이고 나중에는 바꿔야 한다.
         System.out.println("Origin : "+origin);
 
-        response.setHeader("Access-Control-Allow-Origin", origin);
-        response.setHeader("Access-Control-Expose-Headers", "Authorization"); // javascript에서 인증 접근 가능?
-        response.setHeader("Access-Control-Allow-Methods", "POST, PUT, PATCH, GET, DELETE, OPTIONS");
-        response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers", "Origin, X-Key, Content-Type, Accept, Authorization");
-        response.setHeader("Access-Control-Allow-Credentials", "true");
+        response.setHeader("Access-Control-Allow-Origin", origin); // Credentials true면 *로 설정 불가
+        response.setHeader("Access-Control-Expose-Headers", "Authorization"); // 인증 헤더를 노출할 수 있도록 설정
+        response.setHeader("Access-Control-Allow-Methods", "POST, PUT, PATCH, GET, DELETE, OPTIONS"); // OPTIONS 필수
+        response.setHeader("Access-Control-Max-Age", "3600"); // 프리플라이트 요청 결과를 캐시할 시간
+        response.setHeader("Access-Control-Allow-Headers", "Origin, X-Key, Content-Type, Authorization"); // 클라이언트가 서버로 요청할 때 포함할 수 있는 헤더를 지정합니다.
+        response.setHeader("Access-Control-Allow-Credentials", "true"); // true하면 쿠키랑 Authorization 전달 가능
 
         // Preflight 요청을 허용하고 바로 응답하는 코드
         if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
-            response.setStatus(HttpServletResponse.SC_OK);
+            response.setStatus(HttpServletResponse.SC_OK); // options 요청이 오면 200ok 보냄
         }else {
             chain.doFilter(req, res);
         }
